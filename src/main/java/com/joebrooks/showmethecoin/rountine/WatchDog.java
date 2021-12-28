88328395c4b4527ee6fda566a3fbad2205e7e9c5
joebrooks <lcw3176@naver.com>
2021-12-28 16:27:44 +0900

    임시 테스트 코드

--- src/main/java/com/joebrooks/showmethecoin/rountine/WatchDog.java
+++ src/main/java/com/joebrooks/showmethecoin/rountine/WatchDog.java
@@ -1,9 +1,7 @@
 package com.joebrooks.showmethecoin.rountine;
 
-import com.joebrooks.showmethecoin.domain.DailyCoinScore;
 import com.joebrooks.showmethecoin.service.CoinService;
 import com.joebrooks.showmethecoin.service.MyInfoService;
-import com.joebrooks.showmethecoin.service.SlackService;
 import com.joebrooks.showmethecoin.util.OrderParseUtil;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
@@ -11,11 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 @Component
@@ -24,8 +19,6 @@ public class WatchDog {
 
     private float tradePrice = 2235;
     private float totalVolume = 0;
-//    private int timer = 0;
-//    private float adder = (float) 0.005;
     private int maxOrder = 3;
     private int orderCount = 0;
     private float firstTradePrice = 0;
@@ -64,73 +57,59 @@ public class WatchDog {
     public void observingMachine() throws ParseException, UnsupportedEncodingException, NoSuchAlgorithmException {
 
         float nowPrice = coinService.getPrice(nowCoin);
-        float myMoney = myInfoService.getLeftMoney();
-//        isOrdering = true;
-//        float adjust = nowPrice * adder;
-//        float adjustedSellPrice = (float)(Math.ceil(tradePrice + adjust) - Math.ceil(tradePrice + adjust) % 10);
+        float myMoney = myInfoService.getMoneyBalance();
+        float balanceMyCoin =  myInfoService.getCoinBalance(nowCoin);
 
-        if(isCancelling){
-            return;
-        }
-
-
-        if(orderCount < maxOrder && myMoney >= minPrice){  // 구매시점
-//            float adjustedBuyPrice = (float) (Math.ceil(tradePrice - adjust) - Math.ceil(tradePrice - adjust) % 10);
-
-            if(sellStack.size() > 0){
-                ResponseEntity<String> orderInfo = myInfoService.getOrderInfo(sellStack.peek());
-
-                if(OrderParseUtil.isOrderComplete(orderInfo)){
-                    sellStack.clear();
-                } else {
-                    return;
-                }
-            }
-
-            if(tradePrice - threshold >= nowPrice){
-                float volume = (float)(Math.ceil(minPrice / nowPrice * 100000) / 100000);
-
-                String uuid = coinService.buy(nowCoin, volume, nowPrice);
-
-                if(orderCount == 0){
-                    firstTradePrice = nowPrice;
-                }
-
-                totalVolume += volume;
-                orderStack.add(uuid);
-//                messageQueue.add(uuid);
-
-                tradePrice = nowPrice;
-
-                DailyCoinScore.setBuyingMoney(DailyCoinScore.getBuyingMoney() + (volume * nowPrice));
-
-                orderCount++;
-            }
-
-
-        } else if(orderCount >= maxOrder && nowPrice > firstTradePrice + (threshold * 2)) {      // 판매시점
-            ResponseEntity<String> orderInfo = myInfoService.getOrderInfo(orderStack.peek());
-
-            if(OrderParseUtil.isOrderComplete(orderInfo)){
-                String uuid = coinService.sell(nowCoin, totalVolume, nowPrice);
-                totalVolume = 0;
-                orderStack.clear();
-//                    messageQueue.add(orderQueue.poll());
-                DailyCoinScore.setSellingMoney(DailyCoinScore.getSellingMoney() + (totalVolume * nowPrice));
-
-                tradePrice = firstTradePrice;
-//                    tradePrice = nowPrice;
-//                    timer = 0;
-                orderCount = 0;
-
-                sellStack.add(uuid);
-            }
-        }
-
-//        isOrdering = false;
-//        timer++;
-    }
+        System.out.println("now : " + nowPrice);
+        System.out.println("myMoney : " + myMoney);
+        System.out.println("balanceCoin : " + balanceMyCoin);
+//        if(isCancelling){
+//            return;
+//        }
 //
+//
+//        if(orderCount < maxOrder && myMoney >= minPrice){  // 구매시점
+//
+//            if(sellStack.size() > 0){
+//                ResponseEntity<String> orderInfo = myInfoService.getOrderInfo(sellStack.peek());
+//
+//                if(OrderParseUtil.isOrderComplete(orderInfo)){
+//                    sellStack.clear();
+//                } else {
+//                    return;
+//                }
+//            }
+//
+//            if(tradePrice - threshold >= nowPrice){
+//                float volume = (float)(Math.ceil(minPrice / nowPrice * 100000) / 100000);
+//
+//                String uuid = coinService.buy(nowCoin, volume, nowPrice);
+//
+//                if(orderCount == 0){
+//                    firstTradePrice = nowPrice;
+//                }
+//
+//                orderStack.add(uuid);
+//                tradePrice = nowPrice;
+//                orderCount++;
+//            }
+//
+//
+//        } else if(orderCount >= maxOrder && nowPrice > firstTradePrice + (threshold * 2)) {      // 판매시점
+//            ResponseEntity<String> orderInfo = myInfoService.getOrderInfo(orderStack.peek());
+//
+//            if(OrderParseUtil.isOrderComplete(orderInfo)){
+//                float coinBalance = myInfoService.getCoinBalance(nowCoin);
+//                String uuid = coinService.sell(nowCoin, coinBalance, nowPrice);
+//                orderStack.clear();
+//                tradePrice = firstTradePrice;
+//                orderCount = 0;
+//
+//                sellStack.add(uuid);
+//            }
+//        }
+    }
+
 //    @Scheduled(fixedDelay = 5000)
 //    public void messageMachine() throws UnsupportedEncodingException, NoSuchAlgorithmException, ParseException {
 //
