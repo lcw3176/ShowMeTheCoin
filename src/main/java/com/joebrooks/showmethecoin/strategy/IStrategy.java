3e99059dffe40e11bf887a4b86360d39d9e06e0d
joebrooks <lcw3176@naver.com>
2022-03-03 19:50:53 +0900

    삭제

--- src/main/java/com/joebrooks/showmethecoin/strategy/IStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.joebrooks.showmethecoin.strategy;
-
-import com.joebrooks.showmethecoin.common.upbit.CoinType;
-
-public interface IStrategy {
-    <T> Strategy execute(CoinType coinType);
-}
--- src/main/java/com/joebrooks/showmethecoin/strategy/StrategyType.java
+++
@@ -1,8 +0,0 @@
-package com.joebrooks.showmethecoin.strategy;
-
-import lombok.Getter;
-
-@Getter
-public class StrategyType {
-    public static final String RSI = "rsi";
-}
