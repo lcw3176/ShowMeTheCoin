a0ade93d8d309d07fc657713daa64be3a966d7bf
joebrooks <lcw3176@naver.com>
2022-03-08 19:56:17 +0900

    CheckOrder 추가

---
+++ src/main/java/com/joebrooks/showmethecoin/order/CheckOrderResponse.java
@@ -0,0 +1,43 @@
+package com.joebrooks.showmethecoin.order;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class CheckOrderResponse {
+
+    @JsonProperty("uuid")
+    private String uuid;
+
+    @JsonProperty("side")
+    private String side;
+
+    @JsonProperty("price")
+    private String price;
+
+    @JsonProperty("market")
+    private String market;
+
+    @JsonProperty("avg_price")
+    private String avgPrice;
+
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    // 체결된 양
+    @JsonProperty("executed_volume")
+    private String executeVolume;
+
+    // 거래에 사용중인 비용
+    @JsonProperty("locked")
+    private String locked;
+
+    // 해당 주문에 걸린 체결 수
+    @JsonProperty("trade_count")
+    private int tradeCount;
+}
