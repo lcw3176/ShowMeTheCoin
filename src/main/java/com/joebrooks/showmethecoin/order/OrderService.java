bd4972cc6f0df64821dbb430a96229bb48e56e4d
joebrooks <lcw3176@naver.com>
2022-03-09 17:40:17 +0900

    패키지 임포트 경로 변경

--- src/main/java/com/joebrooks/showmethecoin/order/OrderService.java
+++ src/main/java/com/joebrooks/showmethecoin/order/OrderService.java
@@ -1,8 +1,12 @@
 package com.joebrooks.showmethecoin.order;
 
-import com.joebrooks.showmethecoin.common.upbit.UpBitClient;
+import com.joebrooks.showmethecoin.global.upbit.UpBitClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +16,10 @@ public class OrderService {
 
     public OrderResponse requestOrder(OrderRequest request){
         return upBitClient.requestOrder(request, path);
+    }
+
+    public List<CheckOrderResponse> checkOrder(CheckOrderRequest request){
+        return new LinkedList<>(Arrays.asList(upBitClient.checkOrder(request, path)));
     }
 
 }
