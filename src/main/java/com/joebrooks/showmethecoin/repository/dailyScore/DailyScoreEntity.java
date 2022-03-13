fe8182e5c1d48ae031692319f05d12e4e6fb2ff8
joebrooks <lcw3176@naver.com>
2022-03-13 16:09:17 +0900

    하루 성과

---
+++ src/main/java/com/joebrooks/showmethecoin/repository/dailyScore/DailyScoreEntity.java
@@ -0,0 +1,29 @@
+package com.joebrooks.showmethecoin.repository.dailyScore;
+
+import com.joebrooks.showmethecoin.repository.BaseTimeEntity;
+import com.joebrooks.showmethecoin.repository.user.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "daily_score")
+public class DailyScoreEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "today_earn_price")
+    private long todayEarnPrice;
+
+    @ManyToOne(targetEntity = UserEntity.class)
+    @JoinColumn(name = "user_id")
+    private UserEntity userId;
+}
---
+++ src/main/java/com/joebrooks/showmethecoin/repository/dailyScore/DailyScoreRepository.java
@@ -0,0 +1,11 @@
+package com.joebrooks.showmethecoin.repository.dailyScore;
+
+import com.joebrooks.showmethecoin.repository.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DailyScoreRepository extends JpaRepository<DailyScoreEntity, Long> {
+
+    List<DailyScoreEntity> findAllByUserId(UserEntity user);
+}
