b41ab5ec55fc802ca3af981d78da8fdf846f9309
joebrooks <lcw3176@naver.com>
2022-03-15 17:18:30 +0900

    서버 정지시 유저 거래상태 업데이트

--- src/main/java/com/joebrooks/showmethecoin/ShowMeTheCoinApplication.java
+++ src/main/java/com/joebrooks/showmethecoin/ShowMeTheCoinApplication.java
@@ -1,18 +1,24 @@
 package com.joebrooks.showmethecoin;
 
+import com.joebrooks.showmethecoin.repository.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.mail.MessagingException;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
+@RequiredArgsConstructor
 public class ShowMeTheCoinApplication {
+
+    private final UserService userService;
 
     @PostConstruct
     public void setTimeZone(){
@@ -22,5 +28,11 @@ public class ShowMeTheCoinApplication {
     public static void main(String[] args) throws MessagingException {
         SpringApplication.run(ShowMeTheCoinApplication.class, args);
     }
+
+    @PreDestroy
+    public void close(){
+        userService.stopAllUsersTrade();
+    }
+
 
 }
