9657afc7b6b81e29a2f01fec1b0b2efee8fbe728
joebrooks <lcw3176@naver.com>
2022-03-15 14:44:16 +0900

    서버 시간 설정

--- src/main/java/com/joebrooks/showmethecoin/ShowMeTheCoinApplication.java
+++ src/main/java/com/joebrooks/showmethecoin/ShowMeTheCoinApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
 import javax.mail.MessagingException;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
 public class ShowMeTheCoinApplication {
 
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) throws MessagingException {
         SpringApplication.run(ShowMeTheCoinApplication.class, args);
