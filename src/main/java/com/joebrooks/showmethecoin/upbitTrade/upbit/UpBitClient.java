ec84cd7267658f2b06ae440fde4a887c34ab4e8d
joebrooks <lcw3176@naver.com>
2022-03-15 21:57:08 +0900

    불필요한 코드 제거

--- src/main/java/com/joebrooks/showmethecoin/upbitTrade/upbit/UpBitClient.java
+++ src/main/java/com/joebrooks/showmethecoin/upbitTrade/upbit/UpBitClient.java
@@ -2,9 +2,6 @@ package com.joebrooks.showmethecoin.upbitTrade.upbit;
 
 import com.google.gson.Gson;
 import com.joebrooks.showmethecoin.global.httpClient.ClientConfig;
-import com.joebrooks.showmethecoin.global.log.LoggingService;
-import com.joebrooks.showmethecoin.upbitTrade.order.CheckOrderRequest;
-import com.joebrooks.showmethecoin.upbitTrade.order.CheckOrderResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -24,7 +21,6 @@ public class UpBitClient {
     private final String secretKey = System.getenv("secretKey");
 
     private final ClientConfig client;
-    private final LoggingService loggingService;
 
     @Value("${upbit.client.timeout}")
     private int timeoutMillis;
@@ -44,7 +40,6 @@ public class UpBitClient {
                     .acceptCharset(StandardCharsets.UTF_8)
                     .retrieve()
                     .onStatus(HttpStatus::isError, response -> {
-                        loggingService.logResponse(response);
                         return Mono.error(new IllegalStateException("failed get"));
                     })
                     .bodyToMono(clazz)
@@ -67,7 +62,6 @@ public class UpBitClient {
                     .acceptCharset(StandardCharsets.UTF_8)
                     .retrieve()
                     .onStatus(HttpStatus::isError, response -> {
-                        loggingService.logResponse(response);
                         return Mono.error(new IllegalStateException("failed get"));
                     })
                     .bodyToMono(clazz)
@@ -91,7 +85,6 @@ public class UpBitClient {
                     .bodyValue(new Gson().toJson(map))
                     .retrieve()
                     .onStatus(HttpStatus::isError, response -> {
-                        loggingService.logResponse(response);
                         return Mono.error(new IllegalStateException("failed post"));
                     })
                     .bodyToMono(clazz)
@@ -101,109 +94,5 @@ public class UpBitClient {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
-
-
-//    public CheckOrderResponse[] checkOrder(CheckOrderRequest orderRequest, String path) {
-//        HashMap<String, String> map = QueryGenerator.getQueryMap(orderRequest);
-//        try{
-//            return client.getClient(baseUrl, timeoutMillis).get()
-//                    .uri(uriBuilder -> uriBuilder.path(path).queryParam("state", orderRequest.getState()).build())
-//                    .accept(MediaType.APPLICATION_JSON)
-//                    .header("Authorization",
-//                            HeaderGenerator.getJwtHeader(accessKey, secretKey, QueryGenerator.convertQueryToHash(map)))
-//                    .acceptCharset(StandardCharsets.UTF_8)
-//                    .retrieve()
-//                    .onStatus(HttpStatus::isError, response -> {
-//                        loggingService.logResponse(response);
-//                        return Mono.error(new IllegalStateException("failed order"));
-//                    })
-//                    .bodyToMono(CheckOrderResponse[].class)
-//                    .block();
-//
-//        } catch(Exception e){
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
-//
-//    }
-
-//    public CandleResponse[] requestCandles(String path) {
-//        try {
-//            return client.getClient(baseUrl, timeoutMillis).get()
-//                    .uri(path)
-//                    .accept(MediaType.APPLICATION_JSON)
-//                    .acceptCharset(StandardCharsets.UTF_8)
-//                    .retrieve()
-//                    .onStatus(HttpStatus::isError, response -> {
-//                        loggingService.logResponse(response);
-//                        return Mono.error(new IllegalStateException("failed candles"));
-//                    })
-//                    .bodyToMono(CandleResponse[].class)
-//                    .block();
-//
-//        } catch (Exception e){
-//          throw new RuntimeException(e.getMessage(), e);
-//        }
-//    }
-//
-//
-//    public AccountResponse[] requestAccountData(String path){
-//
-//        try {
-//            return client.getClient(baseUrl, timeoutMillis).get()
-//                    .uri(path)
-//                    .accept(MediaType.APPLICATION_JSON)
-//                    .header("Authorization", getJwtHeader())
-//                    .acceptCharset(StandardCharsets.UTF_8)
-//                    .retrieve()
-//                    .onStatus(HttpStatus::isError, response -> {
-//                        loggingService.logResponse(response);
-//                        return Mono.error(new IllegalStateException("failed account"));
-//                    })
-//                    .bodyToMono(AccountResponse[].class)
-//                    .block();
-//
-//        } catch (Exception e){
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
-//    }
-
-
-
-//    public OrderResponse requestOrder(OrderRequest order, String path) {
-//        HashMap<String, String> map = queryGenerator.getQueryMap(order);
-//        try{
-//            return client.getClient(baseUrl, timeoutMillis).post()
-//                    .uri(path)
-//                    .accept(MediaType.APPLICATION_JSON)
-//                    .header("Authorization", getJwtHeader(queryGenerator.convertQueryString(map)))
-//                    .acceptCharset(StandardCharsets.UTF_8)
-//                    .bodyValue(new Gson().toJson(map))
-//                    .retrieve()
-//                    .onStatus(HttpStatus::isError, response -> {
-//                        loggingService.logResponse(response);
-//                        return Mono.error(new IllegalStateException("failed order"));
-//                    })
-//                    .bodyToMono(OrderResponse.class)
-//                    .block();
-//
-//        } catch(Exception e){
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
-//
-//    }
-
-
-
-//    public MultiValueMap<String, String> getMaps(Object obj) {
-//        ObjectMapper mapper = new ObjectMapper();
-//        TypeReference<Map<String,String>> typeRef = new TypeReference<Map<String,String >>(){};
-//
-//        Map<String, String> result = mapper.convertValue(obj, typeRef);
-//
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.setAll(result);
-//
-//        return params;
-//    }
 
 }
