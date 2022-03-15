1acc31875fdfdca3ed4fc6a4860810f9bc480bb7
joebrooks <lcw3176@naver.com>
2022-03-15 14:44:30 +0900

    getalluser 추가

--- src/main/java/com/joebrooks/showmethecoin/repository/user/UserService.java
+++ src/main/java/com/joebrooks/showmethecoin/repository/user/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -32,5 +33,9 @@ public class UserService {
     }
     public void save(UserEntity user){
         userRepository.save(user);
+    }
+
+    public List<UserEntity> getAllUser(){
+        return userRepository.findAll();
     }
 }
