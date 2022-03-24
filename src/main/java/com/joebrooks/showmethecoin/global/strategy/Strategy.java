61188fee170677a733a68bf8bab17352f6d884bd
joebrooks <lcw3176@naver.com>
2022-03-24 14:09:14 +0900

    조건 변경

--- src/main/java/com/joebrooks/showmethecoin/global/strategy/Strategy.java
+++ src/main/java/com/joebrooks/showmethecoin/global/strategy/Strategy.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Strategy {
-    RISING("거래량 높음 / 거래당 수익 낮음",40, 60),
+    RISING("거래량 높음 / 거래당 수익 낮음",35, 55),
     STAY("거래량 보통 / 거래당 수익 보통",30, 60),
     FALLING("거래량 낮음 / 거래당 수익 높음", 20, 60);
 
