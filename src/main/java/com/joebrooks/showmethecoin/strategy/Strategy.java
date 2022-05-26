a167f65fcc8fa070ae61c830271ab97fd9c5caa4
lcw3176 <lcw3176@naver.com>
2022-05-26 23:54:53 +0900

    전략 추가

--- src/main/java/com/joebrooks/showmethecoin/strategy/Strategy.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/Strategy.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Strategy {
 
+    ADX_DMI_STRATEGY("Adx, Dmi 전략"),
     PRICE_STRATEGY("가격 전략"),
     TAIL_STRATEGY("꼬리 전략"),
     QUOTE_STRATEGY("호가 전략");
--- src/main/java/com/joebrooks/showmethecoin/strategy/StrategyAnnotation.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/StrategyAnnotation.java
@@ -4,5 +4,6 @@ public class StrategyAnnotation {
     public static final String PRICE_STRATEGY = "PRICE_STRATEGY";
     public static final String QUOTE_STRATEGY = "QUOTE_STRATEGY";
     public static final String TAIL_STRATEGY = "TAIL_STRATEGY";
+    public static final String ADX_DMI_STRATEGY = "ADX_DMI_STRATEGY";
 
 }
