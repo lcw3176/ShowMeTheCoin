492edf24b879dfd72c760ac836e15ab6dfc56adc
lcw3176 <lcw3176@naver.com>
2022-05-31 23:54:46 +0900

    인터페이스 수정

--- src/main/java/com/joebrooks/showmethecoin/strategy/IStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/IStrategy.java
@@ -2,13 +2,35 @@ package com.joebrooks.showmethecoin.strategy;
 
 import com.joebrooks.showmethecoin.trade.TradeInfo;
 import com.joebrooks.showmethecoin.trade.upbit.candles.CandleResponse;
-import com.joebrooks.showmethecoin.trade.upbit.indicator.type.IndicatorType;
 
 import java.util.List;
 
 public interface IStrategy {
+    double lossRate = 0.1;
+    double gainRate = 0.05;
 
     boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo);
-    boolean isProperToSellWithBenefit(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo);
-    boolean isProperToSellWithLoss(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo);
+    default boolean isProperToSellWithBenefit(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo){
+        double averagePrice = getAveragePrice(tradeInfo);
+
+        return (averagePrice * 1.0005) * 1.0005 < candleResponses.get(0).getTradePrice(); // 실 매도시 인덱스 +1;
+    }
+
+    default boolean isProperToSellWithLoss(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo){
+        double averagePrice = getAveragePrice(tradeInfo);
+
+        return (averagePrice * 1.0005) * (1 - lossRate) > candleResponses.get(0).getTradePrice(); // 실 매도시 인덱스 +1
+    }
+
+    default double getAveragePrice(List<TradeInfo> tradeInfo){
+        double price = 0;
+        double volume = 0;
+
+        for(int i = 0; i < tradeInfo.size(); i++){
+            price += tradeInfo.get(i).getTradePrice() * tradeInfo.get(i).getCoinVolume() * 1.0005;
+            volume += tradeInfo.get(i).getCoinVolume();
+        }
+
+        return price / volume;
+    }
 }
