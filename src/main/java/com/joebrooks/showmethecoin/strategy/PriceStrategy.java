43d15cd188efdd53e20365f196fe974c549cbe62
lcw3176 <lcw3176@naver.com>
2022-05-31 23:54:24 +0900

    가격 전략 수정

--- src/main/java/com/joebrooks/showmethecoin/strategy/PriceStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/PriceStrategy.java
@@ -16,53 +16,11 @@ public class PriceStrategy implements IStrategy{
 
     private final int buyCount = 3;
     private final int sellCount = 4;
-    private final double lossRate = 0.02;
-    private final double gainRate = 0.005;
 
-    @Override
-    public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
-        int count = 0;
-        CandleResponse mostRecentCandle = candleResponses.get(0); // 실 매수시 인덱스 +1
-
-        if(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice() > 0){
-            return false;
-        }
-
-        if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(mostRecentCandle.getDateKst())){
-            return false;
-        }
-
-        double priceGap = Math.abs(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice());
-
-        for(int i = 1; i < candleResponses.size(); i++){ // 실 매수시 i +1
-            CandleResponse candle = candleResponses.get(i);
-            double price = candle.getTradePrice() - candle.getOpeningPrice();
-            
-
-            if(price < 0 && Math.abs(price) > priceGap * 0.8) { // 음봉의 크기가 유의미하게 큰 경우
-                priceGap = Math.abs(price);
-                count++;
-            }
-
-            if(price > 0 && price > priceGap * 0.8){ // 양봉의 크기가 유의미하게 큰 경우
-                break;
-            }
-
-            if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(candle.getDateKst())){
-                break;
-            }
-        }
-
-
-        return count >= buyCount;
-    }
-
-    @Override
-    public boolean isProperToSellWithBenefit(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
+//    @Override
+//    public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
 //        int count = 0;
-        double averagePrice = getAveragePrice(tradeInfo);
-
-//        CandleResponse mostRecentCandle = candleResponses.get(1);
+//        CandleResponse mostRecentCandle = candleResponses.get(0); // 실 매수시 인덱스 +1
 //
 //        if(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice() > 0){
 //            return false;
@@ -74,18 +32,18 @@ public class PriceStrategy implements IStrategy{
 //
 //        double priceGap = Math.abs(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice());
 //
-//        for(int i = 2; i < candleResponses.size(); i++){
+//        for(int i = 1; i < candleResponses.size(); i++){ // 실 매수시 i +1
 //            CandleResponse candle = candleResponses.get(i);
 //            double price = candle.getTradePrice() - candle.getOpeningPrice();
 //
 //
-//            if(price < 0 && Math.abs(price) > priceGap * 0.7) { // 음봉의 크기가 유의미하게 큰 경우
-//                break;
-//            }
-//
-//            if(price > 0 && price > priceGap * 0.7){ // 양봉의 크기가 유의미하게 큰 경우
+//            if(price < 0 && Math.abs(price) > priceGap * 0.8) { // 음봉의 크기가 유의미하게 큰 경우
 //                priceGap = Math.abs(price);
 //                count++;
+//            }
+//
+//            if(price > 0 && price > priceGap * 0.8){ // 양봉의 크기가 유의미하게 큰 경우
+//                break;
 //            }
 //
 //            if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(candle.getDateKst())){
@@ -93,28 +51,18 @@ public class PriceStrategy implements IStrategy{
 //            }
 //        }
 //
-// * (1 + gainRate)
-//        return count >= sellCount && averagePrice + paidFee + getPayingFee(tradeInfo, candleResponses.get(0).getTradePrice()) < candleResponses.get(0).getTradePrice();
-        return (averagePrice * 1.0005) < candleResponses.get(0).getTradePrice();
-    }
+//
+//        return count >= buyCount;
+//    }
 
     @Override
-    public boolean isProperToSellWithLoss(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
-        double averagePrice = getAveragePrice(tradeInfo);
-
-        return (averagePrice * 1.0005) * (1 - lossRate) > candleResponses.get(0).getTradePrice(); // 실 매도시 인덱스 +1
-    }
-
-    private double getAveragePrice(List<TradeInfo> tradeInfo){
-        double price = 0;
-        double volume = 0;
-
-        for(int i = 0; i < tradeInfo.size(); i++){
-            price += tradeInfo.get(i).getTradePrice() * tradeInfo.get(i).getCoinVolume() * 1.0005;
-            volume += tradeInfo.get(i).getCoinVolume();
+    public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
+        if(tradeInfo.size() > 0){
+            return tradeInfo.get(tradeInfo.size() - 1).getTradePrice() > candleResponses.get(0).getTradePrice();
         }
 
-        return price / volume;
+        return true;
     }
+
 
 }
