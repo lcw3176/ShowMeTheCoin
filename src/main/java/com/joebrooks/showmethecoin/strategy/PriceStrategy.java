96452c18c4a1fea28318baf0dd9ed240e8ce443d
ChanwooLee <lcw3176@naver.com>
2022-06-02 14:55:42 +0900

    전략 수정

--- src/main/java/com/joebrooks/showmethecoin/strategy/PriceStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/PriceStrategy.java
@@ -17,52 +17,52 @@ public class PriceStrategy implements IStrategy{
     private final int buyCount = 3;
     private final int sellCount = 4;
 
-//    @Override
-//    public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
-//        int count = 0;
-//        CandleResponse mostRecentCandle = candleResponses.get(0); // 실 매수시 인덱스 +1
-//
-//        if(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice() > 0){
-//            return false;
-//        }
-//
-//        if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(mostRecentCandle.getDateKst())){
-//            return false;
-//        }
-//
-//        double priceGap = Math.abs(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice());
-//
-//        for(int i = 1; i < candleResponses.size(); i++){ // 실 매수시 i +1
-//            CandleResponse candle = candleResponses.get(i);
-//            double price = candle.getTradePrice() - candle.getOpeningPrice();
-//
-//
-//            if(price < 0 && Math.abs(price) > priceGap * 0.8) { // 음봉의 크기가 유의미하게 큰 경우
-//                priceGap = Math.abs(price);
-//                count++;
-//            }
-//
-//            if(price > 0 && price > priceGap * 0.8){ // 양봉의 크기가 유의미하게 큰 경우
-//                break;
-//            }
-//
-//            if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(candle.getDateKst())){
-//                break;
-//            }
-//        }
-//
-//
-//        return count >= buyCount;
-//    }
-
     @Override
     public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
-        if(tradeInfo.size() > 0){
-            return tradeInfo.get(tradeInfo.size() - 1).getTradePrice() > candleResponses.get(0).getTradePrice();
+        int count = 0;
+        CandleResponse mostRecentCandle = candleResponses.get(0); // 실 매수시 인덱스 +1
+
+        if(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice() > 0){
+            return false;
         }
 
-        return true;
+        if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(mostRecentCandle.getDateKst())){
+            return false;
+        }
+
+        double priceGap = Math.abs(mostRecentCandle.getTradePrice() - mostRecentCandle.getOpeningPrice());
+
+        for(int i = 1; i < candleResponses.size(); i++){ // 실 매수시 i +1
+            CandleResponse candle = candleResponses.get(i);
+            double price = candle.getTradePrice() - candle.getOpeningPrice();
+
+
+            if(price < 0 && Math.abs(price) > priceGap * 0.7) { // 음봉의 크기가 유의미하게 큰 경우
+                priceGap = Math.abs(price);
+                count++;
+            }
+
+            if(price > 0 && price > priceGap * 0.7){ // 양봉의 크기가 유의미하게 큰 경우
+                break;
+            }
+
+            if(tradeInfo.size() != 0 && tradeInfo.get(tradeInfo.size() - 1).getDateKst().equals(candle.getDateKst())){
+                break;
+            }
+        }
+
+
+        return count >= buyCount;
     }
+
+//    @Override
+//    public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
+//        if(tradeInfo.size() > 0){
+//            return tradeInfo.get(tradeInfo.size() - 1).getTradePrice() > candleResponses.get(0).getTradePrice();
+//        }
+//
+//        return true;
+//    }
 
 
 }
--- src/main/java/com/joebrooks/showmethecoin/strategy/QuoteStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/QuoteStrategy.java
@@ -7,21 +7,34 @@ import com.joebrooks.showmethecoin.trade.upbit.quote.QuoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component(StrategyAnnotation.QUOTE_STRATEGY)
 @RequiredArgsConstructor
 public class QuoteStrategy implements IStrategy{
 
-    private final QuoteService quoteService;
+//    private final QuoteService quoteService;
+//
+//    @Override
+//    public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
+//        QuoteResponse response = quoteService.getQuote(candleResponses.get(0).getMarket());
+//        double ask = response.getTotalAskSize(); // 호가 매도량
+//        double bid = response.getTotalBidSize(); // 호가 매수량
+//
+//        return bid < ask && ask / 2 < bid;
+//    }
 
     @Override
     public boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo) {
-        QuoteResponse response = quoteService.getQuote(candleResponses.get(0).getMarket());
-        double ask = response.getTotalAskSize(); // 호가 매도량
-        double bid = response.getTotalBidSize(); // 호가 매수량
+        if(tradeInfo.size() > 0){
+            return tradeInfo.get(tradeInfo.size() - 1).getTradePrice() > candleResponses.get(0).getTradePrice();
+        }
 
-        return bid < ask && ask / 2 < bid;
+        return true;
     }
 
 }
