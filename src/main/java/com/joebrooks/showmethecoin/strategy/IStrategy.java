345506e75fe8228402e7b52364a1e28b5454f526
lcw3176 <lcw3176@naver.com>
2022-06-03 23:33:55 +0900

    수수료 계산 수정

--- src/main/java/com/joebrooks/showmethecoin/strategy/IStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/IStrategy.java
@@ -1,36 +1,72 @@
 package com.joebrooks.showmethecoin.strategy;
 
+import com.joebrooks.showmethecoin.global.fee.FeeCalculator;
 import com.joebrooks.showmethecoin.trade.TradeInfo;
 import com.joebrooks.showmethecoin.trade.upbit.candles.CandleResponse;
 
 import java.util.List;
 
+
 public interface IStrategy {
-    double lossRate = 0.1;
-    double gainRate = 0.05;
+    double lossRate = 0.01;
+    double gainRate = 0.0005;
 
     boolean isProperToBuy(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo);
     default boolean isProperToSellWithBenefit(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo){
-        double averagePrice = getAveragePrice(tradeInfo);
+        double averageBuyPrice = getAverageBuyPrice(tradeInfo);
+        double paidFee = getPaidFee(tradeInfo);
+        double averageSellPrice = getAverageSellPrice(candleResponses, tradeInfo);
+        double payingFee = getPayingFee(candleResponses, tradeInfo);
 
-        return (averagePrice * 1.0005) * 1.0005 < candleResponses.get(0).getTradePrice(); // 실 매도시 인덱스 +1;
+        return (averageBuyPrice + paidFee + payingFee) < averageSellPrice; // 실 매도시 인덱스 +1;
     }
 
     default boolean isProperToSellWithLoss(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo){
-        double averagePrice = getAveragePrice(tradeInfo);
+        double averageBuyPrice = getAverageBuyPrice(tradeInfo);
+        double paidFee = getPaidFee(tradeInfo);
+        double averageSellPrice = getAverageSellPrice(candleResponses, tradeInfo);
+        double payingFee = getPayingFee(candleResponses, tradeInfo);
 
-        return (averagePrice * 1.0005) * (1 - lossRate) > candleResponses.get(0).getTradePrice(); // 실 매도시 인덱스 +1
+        return (averageBuyPrice + paidFee + payingFee) * (1 - lossRate) > averageSellPrice; // 실 매도시 인덱스 +1
     }
 
-    default double getAveragePrice(List<TradeInfo> tradeInfo){
+    default double getAverageBuyPrice(List<TradeInfo> tradeInfo){
         double price = 0;
-        double volume = 0;
 
-        for(int i = 0; i < tradeInfo.size(); i++){
-            price += tradeInfo.get(i).getTradePrice() * tradeInfo.get(i).getCoinVolume() * 1.0005;
-            volume += tradeInfo.get(i).getCoinVolume();
+        for(TradeInfo i : tradeInfo){
+            price += i.getTradePrice() * i.getCoinVolume();
         }
 
-        return price / volume;
+        return price;
+    }
+
+    default double getPaidFee(List<TradeInfo> tradeInfo){
+        double fee = 0;
+
+        for(TradeInfo i : tradeInfo){
+            fee += FeeCalculator.calculate(i.getTradePrice() * i.getCoinVolume(), i.getCoinVolume());
+        }
+
+        return fee;
+    }
+
+    default double getAverageSellPrice(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo){
+        double volume = 0;
+
+        for(TradeInfo i : tradeInfo){
+            volume += i.getCoinVolume();
+        }
+
+        return volume * candleResponses.get(0).getTradePrice();
+    }
+
+    default double getPayingFee(List<CandleResponse> candleResponses, List<TradeInfo> tradeInfo){
+        double volume = 0;
+
+        for(TradeInfo i : tradeInfo){
+            volume += i.getCoinVolume();
+        }
+
+        return FeeCalculator.calculate(candleResponses.get(0).getTradePrice() * volume, volume);
     }
 }
