f3ed47decc5bb6598435b11fbd8025641711088c
lcw3176 <lcw3176@naver.com>
2022-06-05 23:12:43 +0900

    전략 추가

--- src/main/java/com/joebrooks/showmethecoin/strategy/StrategyType.java
+++ src/main/java/com/joebrooks/showmethecoin/strategy/StrategyType.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum StrategyType {
-    RsiStrategy("RSI 기반 전략"),
-    AdxDmiStrategy("Adx, Dmi 기반 전략"),
+    RsiStrategy("RSI 전략"),
+    AdxDmiStrategy("Adx, Dmi 전략"),
+    CandleStrategy("캔들 전략"),
     PriceStrategy("가격 전략"),
     TailStrategy("꼬리 전략"),
     BaseStrategy("기본 매매 전략"),
