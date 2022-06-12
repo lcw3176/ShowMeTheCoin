94ab10c9bf7c0c261bce1072fe74aa3c4d699a62
lcw3176 <lcw3176@naver.com>
2022-06-12 22:15:57 +0900

    그래프 유틸 수정

--- src/main/java/com/joebrooks/showmethecoin/global/graph/GraphUtil.java
+++ src/main/java/com/joebrooks/showmethecoin/global/graph/GraphUtil.java
@@ -5,27 +5,14 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class GraphUtil {
 
-    private final double threshold = 0.3;
+    private final double threshold = 5;
 
-    public static GraphStatus getStatus(double pastValue, double recentValue){
-        double maxValue = Math.max(pastValue, recentValue);
-        double minValue = Math.min(pastValue, recentValue);
-        double value = maxValue - minValue;
+    public GraphStatus getStatus(double pastValue, double recentValue){
 
         if(recentValue - pastValue > 0){
-            double temp = value / minValue * 100;
-
-            if(temp > threshold){
-                return GraphStatus.STRONG_RISING;
-            }
 
             return GraphStatus.RISING;
         } else if(recentValue - pastValue < 0){
-            double temp = value / maxValue * 100;
-
-            if(temp > threshold){
-                return GraphStatus.STRONG_FALLING;
-            }
 
             return GraphStatus.FALLING;
         } else{
