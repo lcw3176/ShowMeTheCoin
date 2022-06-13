8e3103cccc3014570f199f02090e1efd7c8cd7a1
lcw3176 <lcw3176@naver.com>
2022-06-14 01:09:43 +0900

    tostring 추가

--- src/main/java/com/joebrooks/showmethecoin/trade/upbit/candles/CandleResponse.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/upbit/candles/CandleResponse.java
@@ -1,15 +1,13 @@
 package com.joebrooks.showmethecoin.trade.upbit.candles;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ToString
 public class CandleResponse{
 
     @JsonProperty("market")
