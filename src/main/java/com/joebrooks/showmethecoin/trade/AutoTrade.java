bd3c344ff49cec83fcfa4d72e6625b622b33e014
lcw3176 <lcw3176@naver.com>
2022-06-16 21:59:28 +0900

    트레이드 정리

--- src/main/java/com/joebrooks/showmethecoin/trade/AutoTrade.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/AutoTrade.java
@@ -1,16 +1,18 @@
 package com.joebrooks.showmethecoin.trade;
 
 import com.joebrooks.showmethecoin.global.exception.type.AutomationException;
-import com.joebrooks.showmethecoin.global.fee.FeeCalculator;
 import com.joebrooks.showmethecoin.repository.userConfig.UserConfigService;
 import com.joebrooks.showmethecoin.strategy.IStrategy;
-import com.joebrooks.showmethecoin.strategy.StrategyType;
 import com.joebrooks.showmethecoin.strategy.StrategyService;
+import com.joebrooks.showmethecoin.strategy.StrategyType;
+import com.joebrooks.showmethecoin.trade.upbit.account.AccountResponse;
 import com.joebrooks.showmethecoin.trade.upbit.account.AccountService;
 import com.joebrooks.showmethecoin.trade.upbit.candles.CandleResponse;
 import com.joebrooks.showmethecoin.trade.upbit.candles.CandleService;
 import com.joebrooks.showmethecoin.trade.upbit.client.CoinType;
-import com.joebrooks.showmethecoin.trade.upbit.order.OrderService;
+import com.joebrooks.showmethecoin.trade.upbit.client.OrderType;
+import com.joebrooks.showmethecoin.trade.upbit.client.Side;
+import com.joebrooks.showmethecoin.trade.upbit.order.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,6 +20,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -30,28 +33,39 @@ public class AutoTrade {
     private final OrderService orderService;
     private final CandleService candleService;
     private final UserConfigService userConfigService;
-
     private final List<TradeInfo> tradeInfoList = new LinkedList<>();
-    private final List<StrategyType> strategyTypeList = new LinkedList<>();
     private final List<IStrategy> strategy = new LinkedList<>();
     private final StrategyService strategyService;
-
-    private double coinBalance = 0D;
-    private double myBalance = 100000D;
-    private double minCash = myBalance / 2;
+    private volatile double minCash;
+    private volatile boolean wasSold = false;
+    private LocalDateTime lastOrderTime = null;
 
     @PostConstruct
     public void init(){
-//        strategyList.add(Strategy.PRICE_STRATEGY);
-//        strategyList.add(Strategy.TAIL_STRATEGY);
-//        strategyList.add(Strategy.QUOTE_STRATEGY);
-//        strategyList.add(Strategy.ADX_DMI_STRATEGY);
+        List<StrategyType> strategyTypeList = new LinkedList<>();
+        strategyTypeList.add(StrategyType.RsiStrategy);
+        strategyTypeList.add(StrategyType.BaseStrategy);
 
         for(StrategyType i : strategyTypeList){
             strategy.add(strategyService.get(i));
         }
+        initMinCash();
+    }
 
-        minCash *= 0.95;
+
+    private void initMinCash(){
+        int divideNum = 7;
+
+        AccountResponse accountResponse = accountService.getKRWCurrency();
+        double myBalance = Math.ceil(Double.parseDouble(accountResponse.getBalance()));
+
+        minCash = Math.ceil(myBalance / divideNum * 0.99);
+    }
+
+    private boolean isOrderDone(){
+        return orderService.checkOrder(CheckOrderRequest.builder()
+                .state(OrderStatus.wait)
+                .build()).size() == 0;
     }
 
     @Scheduled(fixedDelay = 3000)
@@ -63,112 +77,114 @@ public class AutoTrade {
                     return;
                 }
 
+                // 주문 완료 처리
+                if(wasSold && isOrderDone()){
+                    tradeInfoList.clear();
+                    wasSold = false;
+                    initMinCash();
+                }
+
+                // 주문 취소 처리
+                if(lastOrderTime != null && lastOrderTime.plusMinutes(1).isBefore(LocalDateTime.now())){
+                    wasSold = false;
+
+                    List<CheckOrderResponse> checkOrderResponseList = orderService.checkOrder(CheckOrderRequest.builder()
+                            .state(OrderStatus.wait)
+                            .build());
+
+                    for(CheckOrderResponse order : checkOrderResponseList){
+                        orderService.cancelOrder(CancelOrderRequest.builder()
+                                .uuid(order.getUuid())
+                                .build());
+
+                        tradeInfoList.removeIf(info -> info.getUuid().equals(order.getUuid()));
+                    }
+                }
+
+
                 CoinType coinType = user.getTradeCoin();
-                int nowLevel = user.getTradeLevel();
-
-
                 List<CandleResponse> candles = candleService.getCandles(coinType);
 
                 CandleResponse nowCandle = candles.get(0);
+                CandleResponse beforeCandle = candles.get(1);
 
+                // 구매
                 if ((tradeInfoList.size() == 0 || !nowCandle.getDateKst().equals(tradeInfoList.get(tradeInfoList.size() - 1).getDateKst()))
-                        && strategy.stream().allMatch(st -> st.isProperToBuy(candles, tradeInfoList))) { // 구매
-//                    AccountResponse accountResponse = accountService.getKRWCurrency();
+                        && !wasSold
+                        && strategy.stream().allMatch(st -> st.isProperToBuy(candles, tradeInfoList))) {
+                    AccountResponse accountResponse = accountService.getKRWCurrency();
 
 
-//                    double myBalance = Math.ceil(Double.parseDouble(accountResponse.getBalance()));
+                    double myBalance = Math.ceil(Double.parseDouble(accountResponse.getBalance()));
                     // 잔고 체크
                     if (myBalance > minCash) {
-                        double coinVolume = minCash / nowCandle.getTradePrice();
-                        myBalance -= nowCandle.getTradePrice() * coinVolume - FeeCalculator.calculate(nowCandle.getTradePrice(), coinBalance);
-                        coinBalance += coinVolume;
-    //fixme 순서 고칠것
+                        double coinVolume = minCash / beforeCandle.getTradePrice();
+
+                        OrderRequest orderRequest = OrderRequest.builder()
+                                .market(coinType.getName())
+                                .side(Side.bid)
+                                .price(Double.toString(beforeCandle.getTradePrice()))
+                                .volume(BigDecimal.valueOf(coinVolume).toString())
+                                .ordType(OrderType.limit)
+                                .build();
+
+                        OrderResponse orderResponse = orderService.requestOrder(orderRequest);
+
                         TradeInfo tradeInfo = TradeInfo.builder()
-                                .tradeCount(nowLevel)
-                                .tradePrice(nowCandle.getTradePrice())
+                                .tradePrice(beforeCandle.getTradePrice())
                                 .coinVolume(coinVolume)
                                 .dateKst(nowCandle.getDateKst())
+                                .uuid(orderResponse.getUuid())
                                 .build();
 
                         tradeInfoList.add(tradeInfo);
-
-//                        OrderRequest orderRequest = OrderRequest.builder()
-//                                .market(coinType.getName())
-//                                .side(Side.bid)
-//                                .price(Double.toString(nowCandle.getTradePrice()))
-//                                .volume(BigDecimal.valueOf(coinVolume).toString())
-//                                .ordType(OrderType.limit)
-//                                .build();
-//
-//                        orderService.requestOrder(orderRequest);
-                        user.changeTradeLevel(user.getTradeLevel() + 1);
-                        userConfigService.save(user);
-                        log.info("매수 코인:{} 가격:{} 수량:{}",
-                                coinType.getName(),
-                                nowCandle.getTradePrice(),
-                                BigDecimal.valueOf(coinVolume));
+                        lastOrderTime = LocalDateTime.now();
                     }
-                } else if (tradeInfoList.size() > 0
-                        && strategy.stream().allMatch(st -> st.isProperToSellWithBenefit(candles, tradeInfoList))) {  // 익절
 
-//                    AccountResponse coinResponse = accountService.getCoinCurrency(coinType);
-//
-//                    double coinBal = Double.parseDouble(coinResponse.getBalance());
+                }
 
+                // 익절
+                else if (tradeInfoList.size() > 0
+                        && strategy.stream().allMatch(st -> st.isProperToSellWithBenefit(candles, tradeInfoList))) {
+
+                    AccountResponse coinResponse = accountService.getCoinCurrency(coinType);
+                    double coinBalance = Double.parseDouble(coinResponse.getBalance());
 
                     if (coinBalance > 0) {
-                        tradeInfoList.clear();
-//                        OrderRequest orderRequest = OrderRequest.builder()
-//                                .market(coinType.getName())
-//                                .side(Side.ask)
-//                                .price(Double.toString(nowCandle.getTradePrice()))
-//                                .volume(BigDecimal.valueOf(coinBalance).toString())
-//                                .ordType(OrderType.limit)
-//                                .build();
+                        OrderRequest orderRequest = OrderRequest.builder()
+                                .market(coinType.getName())
+                                .side(Side.ask)
+                                .price(Double.toString(beforeCandle.getTradePrice()))
+                                .volume(BigDecimal.valueOf(coinBalance).toString())
+                                .ordType(OrderType.limit)
+                                .build();
 
-//                        orderService.requestOrder(orderRequest);
-                        user.changeTradeLevel(0);
-                        userConfigService.save(user);
-                        myBalance += nowCandle.getTradePrice() * coinBalance - FeeCalculator.calculate(nowCandle.getTradePrice(), coinBalance);
-                        minCash = myBalance / 2;
-                        minCash *= 0.95;
-                        coinBalance = 0D;
-                        log.info("익절 코인:{} 가격:{} 잔고:{}",
-                                coinType.getName(),
-                                nowCandle.getTradePrice(),
-                                myBalance);
+                        orderService.requestOrder(orderRequest);
+                        wasSold = true;
+                        lastOrderTime = LocalDateTime.now();
                     }
-                } else if (tradeInfoList.size() > 0
-                        && strategy.stream().allMatch(st -> st.isProperToSellWithLoss(candles, tradeInfoList))) { // 손절
+                }
 
+                // 손절
+                else if (tradeInfoList.size() > 0
+                        && !nowCandle.getDateKst().equals(tradeInfoList.get(tradeInfoList.size() - 1).getDateKst())
+                        && strategy.stream().allMatch(st -> st.isProperToSellWithLoss(candles, tradeInfoList))) {
 
-//                        AccountResponse coinResponse = accountService.getCoinCurrency(coinType);
-//
-//                        double coinBalance = Double.parseDouble(coinResponse.getBalance());
+                        AccountResponse coinResponse = accountService.getCoinCurrency(coinType);
+                        double coinBalance = Double.parseDouble(coinResponse.getBalance());
+
                     if (coinBalance > 0) {
-                        tradeInfoList.clear();
-//                        OrderRequest orderRequest = OrderRequest.builder()
-//                                .market(coinType.getName())
-//                                .side(Side.ask)
-//                                .price(Double.toString(nowCandle.getTradePrice()))
-//                                .volume(BigDecimal.valueOf(coinBalance).toString())
-//                                .ordType(OrderType.limit)
-//                                .build();
-//
-//                        orderService.requestOrder(orderRequest);
-                        user.changeTradeLevel(0);
+                        OrderRequest orderRequest = OrderRequest.builder()
+                                .market(coinType.getName())
+                                .side(Side.ask)
+                                .price(Double.toString(beforeCandle.getTradePrice()))
+                                .volume(BigDecimal.valueOf(coinBalance).toString())
+                                .ordType(OrderType.limit)
+                                .build();
 
-                        userConfigService.save(user);
-                        myBalance += nowCandle.getTradePrice() * coinBalance - FeeCalculator.calculate(nowCandle.getTradePrice(), coinBalance);
-                        minCash = myBalance / 2;
-                        minCash *= 0.95;
-                        coinBalance = 0D;
-
-                        log.info("손절 코인:{} 가격:{} 잔고:{}",
-                                coinType.getName(),
-                                nowCandle.getTradePrice(),
-                                myBalance);
-
+                        orderService.requestOrder(orderRequest);
+                        wasSold = true;
+                        lastOrderTime = LocalDateTime.now();
                     }
                 }
             });
