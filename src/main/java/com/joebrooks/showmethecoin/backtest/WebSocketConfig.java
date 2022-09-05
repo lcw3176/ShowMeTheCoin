a610890543b216db9bbed84d001c886c0f427f53
lcw3176 <lcw3176@naver.com>
2022-09-05 21:31:06 +0900

    sockjs 적용

--- src/main/java/com/joebrooks/showmethecoin/backtest/WebSocketConfig.java
+++ src/main/java/com/joebrooks/showmethecoin/backtest/WebSocketConfig.java
@@ -16,6 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(backTestSocketHandler, "/backtest/upbit");
+        registry.addHandler(backTestSocketHandler, "/backtest/upbit")
+                .withSockJS();
     }
 }
