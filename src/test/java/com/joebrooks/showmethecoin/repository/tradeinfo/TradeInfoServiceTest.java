7e36efdca628fe670a88cf28005a2ade2c61d1ab
lcw3176 <lcw3176@naver.com>
2022-09-08 15:00:17 +0900

    테스트 수정

--- src/test/java/com/joebrooks/showmethecoin/repository/tradeinfo/TradeInfoServiceTest.java
+++ src/test/java/com/joebrooks/showmethecoin/repository/tradeinfo/TradeInfoServiceTest.java
@@ -1,7 +1,6 @@
 package com.joebrooks.showmethecoin.repository.tradeinfo;
 
 import com.joebrooks.showmethecoin.trade.tradeinfo.TradeInfoService;
-import com.joebrooks.showmethecoin.trade.upbit.CoinType;
 import com.joebrooks.showmethecoin.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,17 +22,15 @@ class TradeInfoServiceTest {
 //    }
 
 
-    @Test
-    void 같은코인의_여러_거래내역_중_최신_데이터만_가져오는지(){
-
-        System.out.println(tradeInfoService.getRecentTrade(userService.getUser("hello"), CoinType.SAND).getOrderedAt());
-    }
+//    @Test
+//    void 같은코인의_여러_거래내역_중_최신_데이터만_가져오는지(){
+//
+//        System.out.println(tradeInfoService.getRecentTrade(userService.getUser("hello"), CoinType.SAND).getOrderedAt());
+//    }
 
     @Test
     void 코인_종류만_가져오는지(){
 
-        tradeInfoService.getAllTradeCoins(userService.getUser("hello")).forEach(i -> {
-            System.out.println(i.getKoreanName());
-        });
+        System.out.println(tradeInfoService.getTradeCoinsCount(userService.getUser("cys028")));
     }
 }
