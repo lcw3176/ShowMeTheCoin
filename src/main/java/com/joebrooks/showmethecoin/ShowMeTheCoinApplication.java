e970c39dcc57d1e1fa4cda477224711b2324ae6a
ChanwooLee <lcw3176@naver.com>
2022-09-09 17:30:25 +0900

    어노테이션 수정

--- src/main/java/com/joebrooks/showmethecoin/ShowMeTheCoinApplication.java
+++ src/main/java/com/joebrooks/showmethecoin/ShowMeTheCoinApplication.java
@@ -1,6 +1,5 @@
 package com.joebrooks.showmethecoin;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@RequiredArgsConstructor
 public class ShowMeTheCoinApplication {
 
     public static void main(String[] args) {
