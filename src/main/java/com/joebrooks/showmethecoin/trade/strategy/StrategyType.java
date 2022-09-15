2623d0a7e0fffa97c6a05d79a34c008641b522d6
lcw3176 <lcw3176@naver.com>
2022-09-15 17:33:41 +0900

    분봉 추가

--- src/main/java/com/joebrooks/showmethecoin/trade/strategy/StrategyType.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/StrategyType.java
@@ -1,5 +1,6 @@
 package com.joebrooks.showmethecoin.trade.strategy;
 
+import com.joebrooks.showmethecoin.repository.candlestore.CandleMinute;
 import com.joebrooks.showmethecoin.trade.strategy.type.BaseStrategy;
 import com.joebrooks.showmethecoin.trade.strategy.type.ChaserStrategy;
 import com.joebrooks.showmethecoin.trade.strategy.type.ShortStrategy;
@@ -7,14 +8,16 @@ import lombok.Getter;
 
 @Getter
 public enum StrategyType {
-    BASE("기본적인 매매 전략, 자동으로 포함됨", BaseStrategy.class),
-    CHASER("추격 매매 전략", ChaserStrategy.class),
-    SHORT("단타 전략", ShortStrategy.class);
+    BASE("기본적인 매매 전략, 자동으로 포함됨", BaseStrategy.class, null),
+    CHASER("추격 매매 전략", ChaserStrategy.class, CandleMinute.ONE_M),
+    SHORT("단타 전략", ShortStrategy.class, CandleMinute.FIVE_M);
     private String description;
     private Class clazz;
+    private CandleMinute candleMinute;
 
-    private StrategyType(String description, Class clazz){
+    private StrategyType(String description, Class clazz, CandleMinute candleMinute){
         this.description = description;
         this.clazz = clazz;
+        this.candleMinute = candleMinute;
     }
 }
