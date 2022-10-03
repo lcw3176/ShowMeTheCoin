eee9d8b37b5ec8276f903db66409a77b65797917
ChanwooLee <lcw3176@naver.com>
2022-10-03 16:15:17 +0900

    반올림 수정 및 세부 수치조절

--- src/main/java/com/joebrooks/showmethecoin/trade/strategy/policy/shorts/buy/ShortBuyUsingMacd.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/policy/shorts/buy/ShortBuyUsingMacd.java
@@ -17,7 +17,7 @@ public class ShortBuyUsingMacd implements IBuyPolicy {
 
     private final MacdIndicator macdIndicator;
     private static final int START_INDEX = 1;
-    private static final int WATCH_COUNT = 8;
+    private static final int WATCH_COUNT = 5;
 
     @Override
     public boolean isProperToBuy(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
@@ -29,11 +29,11 @@ public class ShortBuyUsingMacd implements IBuyPolicy {
 
         for(int i = START_INDEX; i < START_INDEX + WATCH_COUNT; i++){
 
-            double latestSignalValue = Math.round((macdResponseList.get(i).getSignal() * 100) / 100);
-            double oldSignalValue = Math.round((macdResponseList.get(i + 1).getSignal() * 100) / 100);
+            double latestSignalValue = Math.round(macdResponseList.get(i).getSignal() * 100) / 100.0;
+            double oldSignalValue = Math.round(macdResponseList.get(i + 1).getSignal() * 100) / 100.0;
 
-            double latestMacdValue = Math.round((macdResponseList.get(i).getMacd() * 100) / 100);
-            double oldMacdValue = Math.round((macdResponseList.get(i + 1).getMacd() * 100) / 100);
+            double latestMacdValue = Math.round(macdResponseList.get(i).getMacd() * 100) / 100.0;
+            double oldMacdValue = Math.round(macdResponseList.get(i + 1).getMacd() * 100) / 100.0;
 
             if(latestSignalValue > oldSignalValue
                     && latestMacdValue > oldMacdValue){
@@ -45,15 +45,15 @@ public class ShortBuyUsingMacd implements IBuyPolicy {
         }
 
         for(int i = START_INDEX; i < START_INDEX + 1; i++){
-            double latestMacdValue = Math.round((macdResponseList.get(i).getMacd() * 100) / 100) ;
-            double latestSignalValue = Math.round((macdResponseList.get(i).getSignal() * 100) / 100);
+            double latestMacdValue = Math.round(macdResponseList.get(i).getMacd() * 100) / 100.0 ;
+            double latestSignalValue = Math.round(macdResponseList.get(i).getSignal() * 100) / 100.0;
 
-            double priceLine = Math.round((candleResponses.get(i).getTradePrice() / 100 / 4 / 4) * 100 / 100);
+            double priceLine = Math.round((candleResponses.get(i).getTradePrice() / 100 / 4 / 4) * 100) / 100.0;
             double macd = Math.abs(latestMacdValue) / priceLine;
             double signal = Math.abs(latestSignalValue) / priceLine;
+            double result = Math.round(Math.min(macd, signal) / Math.max(macd, signal) * 100) / 100.0;
 
-            if (Math.min(macd, signal) / Math.max(macd, signal) <= 0.6
-                && Math.min(macd, signal) / Math.max(macd, signal) >= 0.4) {
+            if (result <= 0.55 && result >= 0.45) {
                 buySignal = true;
                 break;
             }
