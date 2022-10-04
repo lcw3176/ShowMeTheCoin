a8d0ed9a701a6041b46d86b870931b3f0b589f85
lcw3176 <lcw3176@naver.com>
2022-10-04 14:49:24 +0900

    손절 관련 수정

--- src/main/java/com/joebrooks/showmethecoin/trade/strategy/type/ShortStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/type/ShortStrategy.java
@@ -2,12 +2,14 @@ package com.joebrooks.showmethecoin.trade.strategy.type;
 
 
 import com.joebrooks.showmethecoin.repository.candlestore.CandleStoreEntity;
+import com.joebrooks.showmethecoin.repository.tradeinfo.TradeInfoEntity;
 import com.joebrooks.showmethecoin.trade.strategy.IStrategy;
 import com.joebrooks.showmethecoin.trade.strategy.policy.PolicyService;
+import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.buy.ShortBuyCore;
 import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.buy.ShortBuyUsingMacd;
 import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.buy.ShortBuyUsingRsi;
+import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.sell.ShortSellCore;
 import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.sell.ShortSellUsingRsi;
-import com.joebrooks.showmethecoin.repository.tradeinfo.TradeInfoEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -23,7 +25,7 @@ public class ShortStrategy implements IStrategy {
     public boolean isProperToBuy(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
 
         return policyService
-                .getBuyPolicy(ShortBuyUsingMacd.class, ShortBuyUsingRsi.class)
+                .getBuyPolicy(ShortBuyCore.class, ShortBuyUsingMacd.class, ShortBuyUsingRsi.class)
                 .stream()
                 .allMatch(i -> i.isProperToBuy(candleResponses, tradeInfo));
     }
@@ -39,6 +41,9 @@ public class ShortStrategy implements IStrategy {
 
     @Override
     public boolean isProperToSellWithLoss(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
-        return true;
+        return policyService
+                .getSellPolicy(ShortSellCore.class)
+                .stream()
+                .allMatch(i -> i.isProperToSellWithLoss(candleResponses, tradeInfo));
     }
 }
