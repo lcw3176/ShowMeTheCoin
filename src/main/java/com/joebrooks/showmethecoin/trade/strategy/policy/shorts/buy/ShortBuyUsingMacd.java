e591e2b51e460eb958ee95daccaf248d28f47e2d
lcw3176 <lcw3176@naver.com>
2022-10-18 19:41:41 +0900

    수치 및 전략 변경

--- src/main/java/com/joebrooks/showmethecoin/trade/strategy/policy/shorts/buy/ShortBuyUsingMacd.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/policy/shorts/buy/ShortBuyUsingMacd.java
@@ -7,15 +7,18 @@ import com.joebrooks.showmethecoin.trade.indicator.macd.MacdIndicator;
 import com.joebrooks.showmethecoin.trade.indicator.macd.MacdResponse;
 import com.joebrooks.showmethecoin.trade.strategy.policy.IBuyPolicy;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ShortBuyUsingMacd implements IBuyPolicy {
 
     private final MacdIndicator macdIndicator;
+
     private static final int START_INDEX = 2;
 
     @Override
@@ -23,21 +26,32 @@ public class ShortBuyUsingMacd implements IBuyPolicy {
         List<MacdResponse> macdResponseList = macdIndicator.getMacd(candleResponses);
 
         boolean buySignal = false;
+        boolean volumeSignal = false;
+        int risingCount = 0;
+        int fallingCount = 0;
 
-        for(int i = START_INDEX; i < START_INDEX + 1; i++){
+        for(int i = START_INDEX; i < START_INDEX + 5; i++){
             double latestMacdValue = Math.round((macdResponseList.get(i).getMacd() * 100) / 100) ;
             double latestSignalValue = Math.round((macdResponseList.get(i).getSignal() * 100) / 100);
 
-            double priceLine = Math.round((candleResponses.get(i).getTradePrice() / 100 / 4 / 2) * 100 / 100);
+            double oldMacdValue = Math.round((macdResponseList.get(i + 1).getMacd() * 100) / 100) ;
+            double oldSignalValue = Math.round((macdResponseList.get(i + 1).getSignal() * 100) / 100);
+
+            double priceLine = Math.round((candleResponses.get(i).getTradePrice() / 100 / 4) * 100 / 100);
             double macd = latestMacdValue / priceLine;
             double signal = latestSignalValue / priceLine;
             double result = Math.min(macd, signal) / Math.max(macd, signal);
 
-            if (result > 1.0 && result < 1.1
-                || result > 0.8 && result < 0.9) {
+            if (result > 1.6 && result < 1.7 && latestMacdValue < 0) {
                 buySignal = true;
-                break;
             }
+
+            if(latestMacdValue > oldMacdValue && latestMacdValue < 0){
+                risingCount++;
+            } else {
+                fallingCount++;
+            }
+
 
 
 //            if (result > 1.31 && result < 1.36) {
@@ -47,7 +61,7 @@ public class ShortBuyUsingMacd implements IBuyPolicy {
 
         }
 
-
+//
 //        double latestMacdValue = Math.round((macdResponseList.get(1).getMacd() * 100) / 100) ;
 //        double latestSignalValue = Math.round((macdResponseList.get(1).getSignal() * 100) / 100);
 //
@@ -57,10 +71,11 @@ public class ShortBuyUsingMacd implements IBuyPolicy {
 //        double result = Math.min(macd, signal) / Math.max(macd, signal);
 //        log.info("시간: {}, 값: {}", candleResponses.get(1).getDateKst(), result);
 
+
         return buySignal
-                && Math.abs(macdResponseList.get(2).getMacd()) - Math.abs(macdResponseList.get(2).getSignal()) > Math.abs(macdResponseList.get(1).getMacd()) - Math.abs(macdResponseList.get(1).getSignal())
                 && macdResponseList.get(1).getSignal() < 0
-                && macdResponseList.get(1).getMacd() < 0;
+                && macdResponseList.get(1).getMacd() < 0
+                && risingCount > fallingCount;
 
     }
 }
--- src/main/java/com/joebrooks/showmethecoin/trade/strategy/type/ShortStrategy.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/type/ShortStrategy.java
@@ -5,10 +5,10 @@ import com.joebrooks.showmethecoin.repository.candlestore.CandleStoreEntity;
 import com.joebrooks.showmethecoin.repository.tradeinfo.TradeInfoEntity;
 import com.joebrooks.showmethecoin.trade.strategy.IStrategy;
 import com.joebrooks.showmethecoin.trade.strategy.policy.PolicyService;
-import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.buy.ShortBuyUsingAdx;
 import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.buy.ShortBuyUsingMacd;
 import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.buy.ShortBuyUsingRsi;
-import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.sell.ShortSellUsingAdx;
+import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.sell.ShortSellUsingMacd;
+import com.joebrooks.showmethecoin.trade.strategy.policy.shorts.sell.ShortSellUsingRsi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +24,7 @@ public class ShortStrategy implements IStrategy {
     public boolean isProperToBuy(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
 
         return policyService
-                .getBuyPolicy(ShortBuyUsingMacd.class, ShortBuyUsingRsi.class, ShortBuyUsingAdx.class)
+                .getBuyPolicy(ShortBuyUsingMacd.class, ShortBuyUsingRsi.class)
                 .stream()
                 .allMatch(i -> i.isProperToBuy(candleResponses, tradeInfo));
     }
@@ -33,13 +33,16 @@ public class ShortStrategy implements IStrategy {
     public boolean isProperToSellWithBenefit(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
 
         return policyService
-                .getSellPolicy(ShortSellUsingAdx.class)
+                .getSellPolicy(ShortSellUsingRsi.class)
                 .stream()
                 .allMatch(i -> i.isProperToSellWithBenefit(candleResponses, tradeInfo));
     }
 
     @Override
     public boolean isProperToSellWithLoss(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
-        return true;
+        return policyService
+                .getSellPolicy(ShortSellUsingMacd.class)
+                .stream()
+                .allMatch(i -> i.isProperToSellWithLoss(candleResponses, tradeInfo));
     }
 }
