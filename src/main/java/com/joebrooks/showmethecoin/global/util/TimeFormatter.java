c0941fc41904256ff5a84722f3af01d437602aed
lcw3176 <lcw3176@naver.com>
2022-11-14 20:44:25 +0900

    백테스트 수정중

---
+++ src/main/java/com/joebrooks/showmethecoin/global/util/TimeFormatter.java
@@ -0,0 +1,27 @@
+package com.joebrooks.showmethecoin.global.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TimeFormatter {
+
+    private final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public Date convert(String target){
+        try{
+            return format.parse(target);
+        } catch (Exception e){
+            throw new RuntimeException("타임 포맷 변환 에러");
+        }
+    }
+
+    public String convert(Date target){
+        try{
+            return format.format(target);
+        } catch (Exception e){
+            throw new RuntimeException("타임 포맷 변환 에러");
+        }
+    }
+}
