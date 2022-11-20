99c04628af60a377e3369d118847ffb24233c222
lcw3176 <lcw3176@naver.com>
2022-11-20 21:34:03 +0900

    새 전략 추가

--- src/main/java/com/joebrooks/showmethecoin/trade/strategy/StrategyType.java
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/StrategyType.java
@@ -10,7 +10,8 @@ public enum StrategyType {
     CHASER("추격 매매 전략", ChaserStrategy.class, CandleMinute.ONE_M),
     SHORT("단타 전략", ShortStrategy.class, CandleMinute.FIFTH_M),
     RESISTANCE("변곡점 전략", ResistanceStrategy.class, CandleMinute.FIVE_M),
-    RISING("상승 전략", RisingStrategy.class, CandleMinute.FIVE_M);
+    RISING("상승 전략", RisingStrategy.class, CandleMinute.FIVE_M),
+    WAVE("파동 전략", WaveStrategy.class, CandleMinute.FIVE_M);
 
 
     private String description;
---
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/policy/wave/WaveBuyCore.java
@@ -0,0 +1,52 @@
+package com.joebrooks.showmethecoin.trade.strategy.policy.wave;
+
+import com.joebrooks.showmethecoin.global.util.TimeFormatter;
+import com.joebrooks.showmethecoin.repository.candlestore.CandleStoreEntity;
+import com.joebrooks.showmethecoin.repository.tradeinfo.TradeInfoEntity;
+import com.joebrooks.showmethecoin.trade.strategy.policy.IBuyPolicy;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WaveBuyCore implements IBuyPolicy {
+
+    @Override
+    public boolean isProperToBuy(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
+
+        List<CandleStoreEntity> recent = candleResponses.subList(1, (candleResponses.size() + 1) / 2);
+        List<CandleStoreEntity> past = candleResponses.subList((candleResponses.size() + 1) / 2, candleResponses.size());
+
+        CandleStoreEntity recentLowPrices = recent.stream()
+                .min(Comparator.comparing(CandleStoreEntity::getTradePrice)).get();
+
+
+        CandleStoreEntity pastLowPrices =  past.stream()
+                .min(Comparator.comparing(CandleStoreEntity::getTradePrice)).get();
+
+
+        Date recentValue = TimeFormatter.parse(recentLowPrices.getDateKst());
+        Date pastValue = TimeFormatter.parse(pastLowPrices.getDateKst());
+        Date nowValue = TimeFormatter.parse(candleResponses.get(0).getDateKst());
+
+        long diff;
+        long nowDiff;
+        double yLength;
+
+        diff = recentValue.getTime() - pastValue.getTime();
+        nowDiff = nowValue.getTime() - pastValue.getTime();
+        yLength = Math.max(recentLowPrices.getTradePrice(), pastLowPrices.getTradePrice())
+                - Math.min(recentLowPrices.getTradePrice(), pastLowPrices.getTradePrice());
+
+
+        // 30분 기준
+        long xLength = (diff / 1000) / 1800;
+        long nowLength = (nowDiff / 1000) / 1800;
+
+
+        return Math.max(candleResponses.get(0).getTradePrice(), pastLowPrices.getTradePrice())
+                - Math.min(candleResponses.get(0).getTradePrice(), pastLowPrices.getTradePrice())
+                <= yLength / xLength * nowLength  * (1 + 0.005);
+    }
+}
