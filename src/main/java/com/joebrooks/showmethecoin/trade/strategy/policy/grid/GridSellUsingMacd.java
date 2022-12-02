9f06c2ba62bf875ff37d9fcea034949a0d426340
lcw3176 <lcw3176@naver.com>
2022-12-02 20:01:10 +0900

    전략 수정중

---
+++ src/main/java/com/joebrooks/showmethecoin/trade/strategy/policy/grid/GridSellUsingMacd.java
@@ -0,0 +1,32 @@
+package com.joebrooks.showmethecoin.trade.strategy.policy.grid;
+
+import com.joebrooks.showmethecoin.repository.candlestore.CandleStoreEntity;
+import com.joebrooks.showmethecoin.repository.tradeinfo.TradeInfoEntity;
+import com.joebrooks.showmethecoin.trade.indicator.macd.MacdIndicator;
+import com.joebrooks.showmethecoin.trade.indicator.macd.MacdResponse;
+import com.joebrooks.showmethecoin.trade.strategy.policy.ISellPolicy;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GridSellUsingMacd implements ISellPolicy {
+
+    private final MacdIndicator macdIndicator;
+
+
+    @Override
+    public boolean isProperToSellWithBenefit(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
+        List<MacdResponse> macdResponseList = macdIndicator.getMacd(candleResponses);
+
+
+        return  macdResponseList.get(1).getMacd() > macdResponseList.get(1).getSignal()
+                && macdResponseList.get(0).getMacd() < macdResponseList.get(0).getSignal();
+    }
+
+    @Override
+    public boolean isProperToSellWithLoss(List<CandleStoreEntity> candleResponses, List<TradeInfoEntity> tradeInfo) {
+        return true;
+    }
+}
